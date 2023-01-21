c3b07fcc096368e5045d2bb459da4fff4d1b414e
Ivor <ivorgrisel@gmail.com>
2023-01-21 20:16:59 +0100

    Updated Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./packages/standard/svj/**/*.{php,html,js}"],
   theme: {
     extend: {},
   },
