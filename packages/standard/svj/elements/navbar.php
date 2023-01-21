c67e4b1cbb1abb5b59b43302ffc69ac1007b0797
Ivor <ivorgrisel@gmail.com>
2023-01-22 00:27:28 +0100

    Update on menu logic

--- packages/standard/svj/elements/navbar.php
+++ packages/standard/svj/elements/navbar.php
@@ -2,28 +2,28 @@
 	id="navbar" role="navigation" aria-label="main navigation"
     class="items-center h-screen w-screen hidden">
 	<ul class="flex flex-col items-start">
-        <li class="cursor-pointer no-underline text-3xl p-2 transition duration-1000 hover:opacity-30 focus:opacity-30">
+        <li class="cursor-pointer no-underline text-3xl p-2 transition duration-500 hover:opacity-0 focus:opacity-0 opacity-0 -translate-x-full hidden">
             <a class="menu-item-primary menu-item-has-children" href="#">
-                Supervisors <span>></span>
+                Supervisors >
             </a>
         </li>
         <ul class="flex-col items-start flex">
-            <li class="cursor-pointer no-underline text-3xl p-2 transition duration-1000 hover:opacity-30 focus:opacity-30 -translate-x-full">
-                <button class="secondary-menu-back-button"> < Back</button>
+            <li class="cursor-pointer no-underline text-3xl p-2 transition duration-500 hover:opacity-0 focus:opacity-0 -translate-x-full hidden">
+                <button class="menu-item-secondary secondary-menu-back-button"> < Back</button>
             </li>
-            <li class="cursor-pointer no-underline text-3xl p-2 transition duration-1000 hover:opacity-30 focus:opacity-30 -translate-x-full">
+            <li class="cursor-pointer no-underline text-3xl p-2 transition duration-500 hover:opacity-0 focus:opacity-0 -translate-x-full hidden">
                 <a class="menu-item-secondary" href="#">Seline</a>
             </li>
-            <li class="cursor-pointer no-underline text-3xl p-2 transition duration-1000 hover:opacity-30 focus:opacity-30 -translate-x-full">
+            <li class="cursor-pointer no-underline text-3xl p-2 transition duration-500 hover:opacity-0 focus:opacity-0 -translate-x-full hidden">
                 <a class="menu-item-secondary" href="#">Fransje</a>
             </li>
         </ul>
-        <li class="cursor-pointer no-underline text-3xl p-2 transition duration-1000 hover:opacity-30 focus:opacity-30">
+        <li class="cursor-pointer no-underline text-3xl p-2 transition duration-500 hover:opacity-0 focus:opacity-0 -translate-x-full opacity-0 hidden">
             <a class="menu-item-primary" href="#">
                 Aanbod
             </a>
         </li>
-        <li class="cursor-pointer no-underline text-3xl p-2 transition duration-1000 hover:opacity-30 focus:opacity-30">
+        <li class="cursor-pointer no-underline text-3xl p-2 transition duration-500 hover:opacity-0 focus:opacity-0 -translate-x-full opacity-0 hidden">
             <a class="menu-item-primary" href="#">
                 Scholing
             </a>
--- packages/standard/svj/js/menu.js
+++ packages/standard/svj/js/menu.js
@@ -1,24 +1,126 @@
-function toggleMenu(openMenu) {
-    openMenu = !openMenu;
-    toggleMenuOpenSpan = document.getElementById('toggle-menu-open');
-    toggleMenuCloseSpan = document.getElementById('toggle-menu-close');
-    navBar = document.getElementById('navbar');
-    if (openMenu) {
-        toggleMenuCloseSpan.classList.add('block');
-        toggleMenuCloseSpan.classList.remove('hidden');
-        toggleMenuOpenSpan.classList.add('hidden');
-        toggleMenuOpenSpan.classList.remove('block');
-        navBar.classList.remove('hidden');
-        navBar.classList.add('flex');
-    } else {
-        toggleMenuOpenSpan.classList.add('block');
-        toggleMenuOpenSpan.classList.remove('hidden');
-        toggleMenuCloseSpan.classList.add('hidden');
-        toggleMenuCloseSpan.classList.remove('block');
-        navBar.classList.add('hidden');
-        navBar.classList.remove('flex');
-    }
-    return openMenu;
+function toggleMenu(openMenu,toggleHiddenDelay) {
+    return new Promise((resolve,reject) => {
+        openMenu = !openMenu;
+        toggleMenuOpenSpan = document.getElementById('toggle-menu-open');
+        toggleMenuCloseSpan = document.getElementById('toggle-menu-close');
+        navBar = document.getElementById('navbar');
+        if (openMenu) {
+            toggleMenuCloseSpan.classList.add('block');
+            toggleMenuCloseSpan.classList.remove('hidden');
+            toggleMenuOpenSpan.classList.add('hidden');
+            toggleMenuOpenSpan.classList.remove('block');
+            navBar.classList.remove('hidden');
+            navBar.classList.add('flex');
+        } else {
+            toggleMenuOpenSpan.classList.add('block');
+            toggleMenuOpenSpan.classList.remove('hidden');
+            toggleMenuCloseSpan.classList.add('hidden');
+            toggleMenuCloseSpan.classList.remove('block');
+            navBar.classList.add('hidden');
+            navBar.classList.remove('flex');
+        }
+        setTimeout(() => {
+            resolve(openMenu);
+        },toggleHiddenDelay)   
+    })
+}
+
+async function hideVisibleMenuItems(visibleMenuItems,toggleHiddenDelay) {
+    return new Promise((resolve,reject) => {
+        for(let index = 0; index < visibleMenuItems.length; index++) {
+            let visibleMenuItem = visibleMenuItems[index];
+            visibleMenuItemParentListItem = visibleMenuItem.parentElement;
+            visibleMenuItemParentListItem.classList.add('hidden')        
+            visibleMenuItemParentListItem.classList.remove('block')
+        }
+        setTimeout(() => {
+            resolve("hidden old items")
+        },toggleHiddenDelay)
+    })
+}
+
+async function showHiddenMenuItems(hiddenMenuItems,toggleHiddenDelay) {
+    return new Promise((resolve,reject) => {
+        for(let index = 0; index < hiddenMenuItems.length; index++) {
+            let hiddenMenuItem = hiddenMenuItems[index];
+            hiddenMenuItemParentListItem = hiddenMenuItem.parentElement;
+            hiddenMenuItemParentListItem.classList.add('block')
+            hiddenMenuItemParentListItem.classList.remove('hidden')
+        }
+        setTimeout(() => {
+            resolve("showing hidden menu items")
+        },toggleHiddenDelay)
+    })
+}
+
+async function transitionCurrentVisibleMenuItems(currentVisibleMenuItems,switchMenuItemsDelay) {
+    return new Promise((resolve,reject) => {
+        for(let index = 0; index < currentVisibleMenuItems.length; index++) {
+            let currentVisibleMenuItem = currentVisibleMenuItems[index];
+            currentVisibleMenuItemParentListItem = currentVisibleMenuItem.parentElement;
+
+            addClassStrings = ['-translate-x-full','opacity-0','hover:opacity-0','focus:opacity-0']
+            removeClassStrings = ['transform-none','opacity-100','hover:opacity-30','focus:opacity-30']
+            for(let index = 0; index < addClassStrings.length; index++) {
+                currentVisibleMenuItemParentListItem.classList.add(addClassStrings[index])
+            }
+            
+            for(let index = 0; index < removeClassStrings.length; index++) {
+                currentVisibleMenuItemParentListItem.classList.remove(removeClassStrings[index])   
+            }
+        }
+        setTimeout(() => {
+            resolve("done transitioning")
+        },switchMenuItemsDelay);
+    })  
+}
+
+async function transitionNewVisibleMenuItems(newVisibleMenuItems,switchMenuItemsDelay) {
+    return new Promise((resolve,reject) => {
+        for(let index = 0; index < newVisibleMenuItems.length; index++) {
+            let newVisibleMenuItem = newVisibleMenuItems[index];
+            newVisibleMenuItemParentListItem = newVisibleMenuItem.parentElement;
+
+            addClassStrings = ['transform-none','opacity-100','hover:opacity-30','focus:opacity-30']
+            removeClassStrings = ['-translate-x-full','opacity-0','hover:opacity-0','focus:opacity-0']
+            for(let index = 0; index < addClassStrings.length; index++) {
+                newVisibleMenuItemParentListItem.classList.add(addClassStrings[index])
+            }
+            
+            for(let index = 0; index < removeClassStrings.length; index++) {
+                newVisibleMenuItemParentListItem.classList.remove(removeClassStrings[index])   
+            }
+        }
+        setTimeout(() => {
+            resolve("done transitioning")
+        },switchMenuItemsDelay);
+    })  
+}
+
+async function switchVisibleMenuItems (currentVisibleMenuItems,newVisibleMenuItems,toggleHiddenDelay,switchMenuItemsDelay) {
+    return new Promise(async (resolve,reject) => {
+        console.log(currentVisibleMenuItems);
+        console.log(newVisibleMenuItems);
+        if (currentVisibleMenuItems.length > 0 && newVisibleMenuItems.length === 0) {
+            await transitionCurrentVisibleMenuItems(currentVisibleMenuItems,switchMenuItemsDelay)
+            await hideVisibleMenuItems(currentVisibleMenuItems,toggleHiddenDelay);
+        }
+        if (currentVisibleMenuItems.length > 0 && newVisibleMenuItems.length > 0) {
+            console.log("Getting here...");
+            await transitionCurrentVisibleMenuItems(currentVisibleMenuItems,switchMenuItemsDelay)
+            console.log("Going way to fast...");
+            await hideVisibleMenuItems(currentVisibleMenuItems,toggleHiddenDelay);
+            console.log("Hold it!...");
+            await showHiddenMenuItems(newVisibleMenuItems,toggleHiddenDelay);
+            await transitionNewVisibleMenuItems(newVisibleMenuItems,switchMenuItemsDelay);
+        }
+        if (currentVisibleMenuItems.length === 0 && newVisibleMenuItems.length > 0) {
+            await showHiddenMenuItems(newVisibleMenuItems,toggleHiddenDelay);
+            await transitionNewVisibleMenuItems(newVisibleMenuItems,switchMenuItemsDelay);
+        }
+        console.log("Probably getting here too quickly...");
+        resolve(newVisibleMenuItems);
+    })
 }
 
 function togglePrimaryMenuItems(visiblePrimaryMenuItems) {
@@ -98,43 +200,47 @@ function toggleSecondaryMenu(currentSecundaryMenu,newSecundaryMenu) {
     return newSecundaryMenu;
 }
 
-(function() {
-    const slideDelay = 1000;
+(function () {
+    const toggleHiddenDelay = 1;
+    const switchMenuItemsDelay = 500;
     let openMenu = false;
-    let visiblePrimaryMenuItems = true;
-    let secondaryMenu;
-    let secondaryMenuBackButtons = document.getElementsByClassName('secondary-menu-back-button');
+    let visibleMenuItems = [];
     toggleMenuButton = document.getElementById('toggle-menu');
-    menuItemsWithChildren = document.getElementsByClassName('menu-item-has-children');
-
-    toggleMenuButton.addEventListener("click", () => {
-        openMenu = toggleMenu(openMenu);
-        if (openMenu) {
-            visiblePrimaryMenuItems = togglePrimaryMenuItems(false);
-            secondaryMenu = toggleSecondaryMenu(secondaryMenu,undefined);
+    toggleMenuButton.addEventListener("click", async () => {
+        if (!openMenu) {
+            openMenu = await toggleMenu(openMenu,toggleHiddenDelay);
+            primaryMenuItems = document.getElementsByClassName('menu-item-primary');
+            visibleMenuItems = await switchVisibleMenuItems([],primaryMenuItems,toggleHiddenDelay,switchMenuItemsDelay)
+        } else {
+            console.log("Closing menu, so first remove the elements!")
+            visibleMenuItems = await switchVisibleMenuItems(visibleMenuItems,[],toggleHiddenDelay,switchMenuItemsDelay)
+            openMenu = await toggleMenu(openMenu,toggleHiddenDelay);
         }
     });
-
+    
+    const menuItemsWithChildren = document.getElementsByClassName('menu-item-has-children');
     for(let index = 0; index < menuItemsWithChildren.length; index++) {
         menuItemWithChildren = menuItemsWithChildren[index];
-        menuItemWithChildren.addEventListener("click", (event) => {
+        menuItemWithChildren.addEventListener("click", async (event) => {
             clickedMenuItemWithChildren = event.target;
             clickedMenuItemWithChildrenParentListItem = clickedMenuItemWithChildren.parentElement;
-            visiblePrimaryMenuItems = togglePrimaryMenuItems(visiblePrimaryMenuItems);
-            console.log(clickedMenuItemWithChildrenParentListItem);
-            setTimeout(() => {
-                secondaryMenu = toggleSecondaryMenu(secondaryMenu,clickedMenuItemWithChildrenParentListItem.nextElementSibling);
-            }, slideDelay)
+            secondaryMenuList = clickedMenuItemWithChildrenParentListItem.nextElementSibling;
+            secondaryMenuItems = secondaryMenuList.getElementsByClassName('menu-item-secondary');
+            visibleMenuItems = await switchVisibleMenuItems(visibleMenuItems,secondaryMenuItems,toggleHiddenDelay,switchMenuItemsDelay)
         });
     }
 
+    const secondaryMenuBackButtons = document.getElementsByClassName('secondary-menu-back-button');
+    console.log(secondaryMenuBackButtons);
     for(let index = 0; index < secondaryMenuBackButtons.length; index++) {
         secondaryMenuBackButton = secondaryMenuBackButtons[index];
-        secondaryMenuBackButton.addEventListener("click", () => {
-            secondaryMenu = toggleSecondaryMenu(secondaryMenu,undefined);
-            setTimeout(() => {
-                visiblePrimaryMenuItems = togglePrimaryMenuItems(visiblePrimaryMenuItems);
-            }, slideDelay)
+        secondaryMenuBackButton.addEventListener("click", (event) => {
+            secondaryMenuBackButton = event.target;
+            console.log(secondaryMenuBackButton);
+            // secondaryMenuItems = toggleSecondaryMenu(secondaryMenu,undefined);
+            // setTimeout(() => {
+            //     visiblePrimaryMenuItems = togglePrimaryMenuItems(visiblePrimaryMenuItems);
+            // }, slideDelay)
         })
     }
 })();
