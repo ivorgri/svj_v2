43771f3c77760cb072404a916e4dbb5d223fb423
Ivor <ivorgrisel@gmail.com>
2023-08-16 12:00:45 +0200

    Adjusted page not found

--- packages/svj/svj_theme/page_not_found.php
+++ packages/svj/svj_theme/page_not_found.php
@@ -3,7 +3,7 @@
     <section id="page-not-found" class="w-screen flex flex-row items-center justify-center mt-32 mb-16 scroll-mt-mobile-header 
         md:mb-24 md:scroll-mt-header">
         <div class="w-11/12 flex flex-row flex-wrap items-center gap-10 md:w-10/12 md:gap-y-0 md:gap-x-6 md:items-start md:justify-items-center">
-        <h2 class="w-full text-5xl text-svj-green font-thin">
+        <h2 class="w-full text-5xl text-svj-primary font-thin">
             @{ page_not_found_title }
         </h2>
         <p class="w-full px-2 md:w-full md:mt-10 md:px-12">
